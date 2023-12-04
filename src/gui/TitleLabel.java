c2b23fb82176be9134f357e68157001a12e74d70
Ghosttxii <81270787+ExquisiteMuffin@users.noreply.github.com>
2023-12-04 09:48:37 -0500

    Create TitleLabel.java

---
+++ src/gui/TitleLabel.java
@@ -0,0 +1,11 @@
+import javax.swing.JLabel;
+
+public class TitleLabel extends JLabel 
+{
+    public TitleLabel()
+    {
+        this.setText("(N/A)");
+        this.setForeground(Color.BLUE);
+        this.setFont(new Font("MV Boli", Font.PLAIN, 42));
+    }
+}
