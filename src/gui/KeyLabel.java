199bca35b2a83eb3f56533d1f395ed46570e61ce
Ghosttxii <81270787+ExquisiteMuffin@users.noreply.github.com>
2023-12-04 09:50:09 -0500

    Create KeyLabel.java

---
+++ src/gui/KeyLabel.java
@@ -0,0 +1,11 @@
+import javax.swing.JLabel;
+
+public class KeyLabel extends JLabel
+{
+    public KeyLabel()
+    {
+        this.setFont(new Font("MV Boli", Font.PLAIN, 28));
+        this.setText("(N/A)");
+    }
+    }
+}
