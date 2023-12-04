e66f7b82377ca13a1028c2db36900ed4a6d86ff3
Ghosttxii <81270787+ExquisiteMuffin@users.noreply.github.com>
2023-12-04 09:47:25 -0500

    Create NewTextField.java

---
+++ src/gui/NewTextField.java
@@ -0,0 +1,10 @@
+import javax.swing.JTextField;
+
+public class NewTextField extends JTextField
+{
+
+    public NewTextField()
+    {
+        this.setPreferredSize(new Dimension(400, 40));
+    }
+}
