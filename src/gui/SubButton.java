8144ee643a483ef1a33c67e308d5ad96cbf0a199
Ghosttxii <81270787+ExquisiteMuffin@users.noreply.github.com>
2023-12-05 21:39:34 -0500

    Update SubButton.java

--- src/gui/SubButton.java
+++ src/gui/SubButton.java
@@ -1,10 +1,14 @@
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
 import javax.swing.border.Border;
+import javax.swing.JButton;
 
 public class SubButton extends JButton
 {
-    public SubButton()
+    public SubButton(String text)
     {
+        this.setText(text);
         Border border = BorderFactory.createLineBorder(Color.YELLOW);
         this.setBorder(border);
         this.setPreferredSize(new Dimension(300, 100));
