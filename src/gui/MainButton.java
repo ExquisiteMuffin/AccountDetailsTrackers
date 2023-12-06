f9e5ed21f7a64219c66b30c19973cfb36d051130
Ghosttxii <81270787+ExquisiteMuffin@users.noreply.github.com>
2023-12-05 21:38:54 -0500

    Update MainButton.java

--- src/gui/MainButton.java
+++ src/gui/MainButton.java
@@ -1,10 +1,14 @@
 import javax.swing.JButton;
 import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
 
 public class MainButton extends JButton
 {
-    public MainButton()
+    public MainButton(String title)
     {
+        this.setText(title);
         Border border = BorderFactory.createLineBorder(Color.BLUE);
         this.setBorder(border);
         this.setPreferredSize(new Dimension(300, 50));
